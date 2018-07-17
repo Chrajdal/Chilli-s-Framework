ab19b53b17c9db5f49f338b9ca8170e54cd48dc1
Jan Chráska <jan.chraska@gmail.com>
2018-07-17 16:11:42 +0200

    added handleinput

--- chili_framework-master/Engine/Game.cpp
+++ chili_framework-master/Engine/Game.cpp
@@ -2,18 +2,12 @@
 #include "Bitmap.h"
 #include "Font.h"
 
-Bitmap goat1("goat.bmp");
-Bitmap goat2(goat1);
-
 Game::Game(MainWindow & wnd)
 	:
 	wnd(wnd),
 	gfx(wnd)
 {
 	srand(unsigned(time(0)));
-	
-	
-	//goat2.resize(goat1.get_width() / 2, goat1.get_height() / 2);
 }
 
 void Game::Go()
@@ -22,10 +16,16 @@ void Game::Go()
 		wnd.Kill();
 	gfx.BeginFrame();
 
+	HandleInput();
 	UpdateModel();
 	ComposeFrame();
-
+	
 	gfx.EndFrame();
+}
+
+void Game::HandleInput()
+{
+
 }
 
 void Game::UpdateModel()
@@ -33,9 +33,7 @@ void Game::UpdateModel()
 
 }
 
-
 void Game::ComposeFrame()
 {
-	goat1.Draw(gfx, 0, 0, Colors::White);
-	goat2.Draw(gfx, goat1.get_width(), 0, Colors::White);
+
 }
--- chili_framework-master/Engine/Game.h
+++ chili_framework-master/Engine/Game.h
@@ -35,6 +35,7 @@ public:
 private:
 	void ComposeFrame();
 	void UpdateModel();
+	void HandleInput();
 	
 private:
 	MainWindow& wnd;
