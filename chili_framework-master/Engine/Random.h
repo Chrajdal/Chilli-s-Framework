cb4b82cdb6386661889ffc42f4fd314096a47d7e
Chrajdal <jan.chraska@gmail.com>
2018-07-19 19:51:02 +0200

    edit random

--- chili_framework-master/Engine/Random.h
+++ chili_framework-master/Engine/Random.h
@@ -6,14 +6,11 @@
 class Random
 {
 	std::mt19937 rng;
-	std::uniform_int_distribution<int32_t> i_dist;
-	std::uniform_real_distribution<double_t> d_dist;
 public:
 	Random(uint_least32_t seed = time(NULL));
-	Random() : Random(std::_Random_device())
-	{}
-	Random();
+	~Random();
 
 	int32_t next(int32_t minval = std::numeric_limits<int32_t>::min(), int32_t maxval = std::numeric_limits<int32_t>::max());
-	double next_double(double minval = std::numeric_limits<double>::min(), double maxval = std::numeric_limits<double>::max());
+	float_t next_float(float_t minval = std::numeric_limits<float_t>::min(), float_t maxval = std::numeric_limits<float_t>::max());
+	double_t next_double(double_t minval = std::numeric_limits<double_t>::min(), double_t maxval = std::numeric_limits<double_t>::max());
 };
