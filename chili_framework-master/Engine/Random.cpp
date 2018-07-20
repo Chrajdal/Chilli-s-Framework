3e1fd1c9cc0e9a6782f282ab152cd0e30462f833
Jan Chráska <jan.chraska@gmail.com>
2018-07-20 13:55:05 +0200

    fixed random

--- chili_framework-master/Engine/Random.cpp
+++ chili_framework-master/Engine/Random.cpp
@@ -1,23 +1,23 @@
 #include "Random.h"
 
-Random::Random(uint_least32_t seed = time(NULL))
+Random::Random(uint_least32_t seed)
 {
 	rng = std::mt19937(seed);
 }
 
-int32_t Random::next(int32_t minval = std::numeric_limits<int32_t>::min(), int32_t maxval = std::numeric_limits<int32_t>::max())
+int32_t Random::next(int32_t minval, int32_t maxval)
 {
 	std::uniform_int_distribution<int32_t> d(minval, maxval);
 	return d(rng);
 }
 
-float_t Random::next_float(float_t minval = std::numeric_limits<float_t>::min(), float_t maxval = std::numeric_limits<float_t>::max())
+float_t Random::next_float(float_t minval, float_t maxval)
 {
 	std::uniform_real_distribution<double_t> d(minval, maxval);
 	return d(rng);
 }
 
-double_t Random::next_double(double_t minval = std::numeric_limits<double_t>::min(), double_t maxval = std::numeric_limits<double_t>::max())
+double_t Random::next_double(double_t minval, double_t maxval)
 {
 	std::uniform_real_distribution<double_t> d(minval, maxval);
 	return d(rng);
--- chili_framework-master/Engine/Random.h
+++ chili_framework-master/Engine/Random.h
@@ -8,7 +8,7 @@ class Random
 	std::mt19937 rng;
 public:
 	Random(uint_least32_t seed = time(NULL));
-	~Random();
+	~Random() {}
 
 	int32_t next(int32_t minval = std::numeric_limits<int32_t>::min(), int32_t maxval = std::numeric_limits<int32_t>::max());
 	float_t next_float(float_t minval = std::numeric_limits<float_t>::min(), float_t maxval = std::numeric_limits<float_t>::max());
